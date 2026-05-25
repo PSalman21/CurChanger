e3c2f604f2a11d3ab41499976da1d815b1964811
PSalman21 <pogorvsalman21@gmail.com>
2026-05-25 21:19:51 +0300

    Update client.py
    Задаю окно клиентской части

--- client.py
+++ client.py
@@ -4,3 +4,9 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 
 API_URL = "http://127.0.0.1:8000"
+
+class CurrencyConverter(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Конвертер валют")
+        self.setFixedSize(400, 300)
